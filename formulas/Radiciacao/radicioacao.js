ae8dd1003c12435dabcac3b98f79bc96ecad511d
Cleyton Lucas Cabral da Silva <Cleytonlucasiurd@gmail.com>
2022-07-09 01:16:35 -0300

    Upgrade na radiciação

--- formulas/Radiciacao/radicioacao.js
+++ formulas/Radiciacao/radicioacao.js
@@ -1,28 +1,8 @@
 function calcular(){
     valor = document.getElementById('valor').value
-    var result;
-    if(valor == 1){
-        result = 1
-    }else if(valor == 4){
-        result = 2
-    }else if(valor == 9){
-        result = 3
-    }else if(valor == 16){
-        result = 4
-    }else if(valor == 25){
-        result = 5
-    }else if(valor == 36){
-        result = 6
-    }else if(valor == 49){
-        result = 7
-    }else if(valor == 64){
-        result = 8
-    }else if(valor == 81){
-        result = 9
-    }else if(valor == 100){
-        result = 10
-    }
 
-        res.innerHTML = `<span style="background-color: orange;">&#8730;${valor} = ${result}</span>`
+    result = Math.sqrt(valor)
+
+        res.innerHTML = `<span style="background-color: orange;">&#8730;${valor} = ${result.toFixed(2)}</span>`
     
 }
